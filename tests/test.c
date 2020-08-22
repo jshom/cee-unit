172919d145190d168c118f1fe631f2b4135345a6
Jacob Shomstein <jacob@shomstein.com>
2020-08-22 19:00:18 -0400

    adding more tests before moving onto new functionality

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,8 @@
-#include "../CU.h" // you would download CU.h at reference that way
+#include "../CU.h" // you would download CU.h and have in your project
+
 /* test suites */
 #include "./test_suites/CU_test_result.h"
+#include "./test_suites/CU_test_node.h"
 
 CU_test_result* test__always_succeeds() {
     return CU_test_result__new(CU_SUCCESS, (char* ) __FUNCTION__);
@@ -9,9 +11,18 @@ CU_test_result* test__always_succeeds() {
 int main() {
     // create test runner
     CU_test_runner* trnr = CU_test_runner__new();
+
     // add tests
     CU_test_runner__add_test(trnr, &test__always_succeeds);
+
     CU_test_runner__add_test(trnr, &test__CU_test_result__new);
+    CU_test_runner__add_test(trnr, &test__CU_test_result__new__with_failure);
+    CU_test_runner__add_test(trnr, &test__CU_test_result__new__non_zero_or_one);
+
+    CU_test_runner__add_test(trnr, &test__CU_test_node__new);
+    CU_test_runner__add_test(trnr, &test__CU_test_node__run);
+    CU_test_runner__add_test(trnr, &test__CU_test_node__run__with_failed_test);
+
     // run all tests
     return CU_test_runner__exec(trnr);
 }
